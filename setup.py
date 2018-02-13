72e271549c283fdd260de6b7a5024cb9b570b49a
Viktor Adam <rycus86@gmail.com>
2018-02-13 22:05:23 +0000

    Bump version number to 0.0.3

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='ghost_client',
     packages=['ghost_client'],
-    version='0.0.2',
+    version='0.0.3',
     description='API client for the Ghost blogging platform',
     long_description=open('README.md').read(),
     license='MIT',
     author='Viktor Adam',
     author_email='rycus86@gmail.com',
     url='https://github.com/rycus86/ghost-client',
-    download_url='https://github.com/rycus86/ghost-client/archive/0.0.2.tar.gz',
+    download_url='https://github.com/rycus86/ghost-client/archive/0.0.3.tar.gz',
     keywords=['ghost', 'blog', 'api', 'client'],
     classifiers=[
         'Development Status :: 4 - Beta',
